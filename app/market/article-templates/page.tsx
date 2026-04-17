4f6d3770532a00b3c13a6cace1ef918b49126cd2
凤凰70 <730357683@qq.com>
2026-04-17 19:15:31 +0800

    Add cooperation application and cooperation list functionality

--- app/market/article-templates/page.tsx
+++ app/market/article-templates/page.tsx
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿"use client"
+﻿﻿﻿﻿﻿﻿"use client"
 import { useState, useEffect } from "react"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
