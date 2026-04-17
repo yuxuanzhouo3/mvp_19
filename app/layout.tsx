41f5ac86ebe477153636fa39222f88b5ee3f9a10
凤凰70 <730357683@qq.com>
2026-04-17 20:48:34 +0800

    Remove Vercel Analytics for CloudBase deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css"
@@ -51,7 +50,6 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
           <Toaster />
-          <Analytics />
         </ThemeProvider>
       </body>
     </html>
